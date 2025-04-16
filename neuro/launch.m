2c8c35f40311c14f8a501db668c52998607b2a2d
donghaiwang <whd@hutb.edu.cn>
2025-04-16 09:24:02 +0800

    新增Carla建图脚本

--- neuro/launch.m
+++ neuro/launch.m
@@ -3,19 +3,22 @@
 addpath(genpath('.'))
 
 
-%% test_3d_mapping
+%% 测试3D建图 test_3d_mapping
+
+% CarlaData：Carla 的 Town10 场景中
+test_mapping_CarlaData
 
 % QUTCarparkData：QUT 停车场数据
 test_mapping_QUTCarparkData
 
-% SynPanData
+% 全景(Panoramic)相机：SynPanData
 test_mapping_SynPanData
 
-% SynPerData
+% 透视(Perspective)相机：SynPerData
 test_mapping_SynPerData
 
 
-%% test_aidvo
+%% 3D 视觉里程计 aidvo ：test_aidvo
 % QUTCarparkData
 test_vo_QUTCarparkData
 
